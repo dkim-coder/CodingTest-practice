3bb888d3e07e82b9703f2be2af0e291d4dbf2979
dkim-coder <jeus5771@naver.com>
2024-03-15 17:29:30 +0900

    2024-03-15

--- Programmers/pb_42839/main.cpp
+++ Programmers/pb_42839/main.cpp
@@ -1,17 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
+using namespace std;
+
+// 소수 판별 여부 함수 필요
+bool isPrime(int num)
+{
+    if (num <= 1)
+        return false;
+
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+
+    return true;
+}
 
 int solution(string numbers)
 {
     int answer = 0;
+    vector<int> vec_numbers(numbers.length(), 0);
+    vector<int> check(numbers.length(), 0);
+
+    for (auto i : numbers)
+        vec_numbers.push_back(atoi(i));
+
+    for (int i = 1; i <= vec_numbers.size(); i++)
+    {
+        vector<int> vec_tmp_numbers;
+        do
+        {
+        }
+    }
 
     return answer;
 }
 
 int main()
 {
+    string numbers_1 = "17";
+    string numbers_2 = "001";
 
+    cout << solution(numbers_1) << endl;
+    cout << solution(numbers_2) << endl;
 
     return EXIT_SUCCESS;
 }
