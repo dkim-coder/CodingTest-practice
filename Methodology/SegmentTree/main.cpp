2f649fd62b4ea2eabd0698609c90d2cdf385ed66
dkim-coder <jeus5771@naver.com>
2025-02-18 21:12:42 +0900

    세그먼트 트리 정리 필요

---
+++ Methodology/SegmentTree/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+namespace segmenttree{
+    using namespace std;
+
+    void cc(int a){}
+
+    void add(){
+        cc()
+    }
+}
+
+using namespace segmenttree;
+
+
+int main()
+{
+    
+    return EXIT_SUCCESS;
+}
