7dbf802942fd6623cf6bd27287bd429ca02cab45
dkim-coder <jeus5771@naver.com>
2025-02-22 15:43:16 +0900

    Autoever end

--- RemindTest/2025_02_AutoEver/Arrow_Problem/main.cpp
+++ RemindTest/2025_02_AutoEver/Arrow_Problem/main.cpp
@@ -1,73 +1,138 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+#define EXIT_SUCCESS 0
 
-constexpr int INF = 1e9;
+/**
+ * @file main.cpp
+ * @date 2025-02-22
+ * @author jeus5771@naver.com
+ * @version 1.0
+ * @brief 오토에버 화살 문제 복기 및 풀이
+ */
+namespace AutoEver
+{
+    using namespace std;
+
+    template <typename T>
+    class Arrow
+    {
+    private:
+        Arrow() = default;
+        ~Arrow() = default;
+        Arrow(const Arrow &) = delete;
+        Arrow &operator=(const Arrow &) = delete;
+        T N, M, K;                      // 보드 크기 (N x N), 화살 개수, 목표 점수
+        vector<vector<T>> board;        // 보드 점수 정보
+        vector<T> weights;              // 화살의 무게
+        vector<vector<T>> arrow_scores; // 화살이 보드의 맞히는 곳에 따른 획득 점수
+        vector<vector<T>> dp;           // 배낭문제 기반 dp 솔루션
+
+    public:
+        /**
+         * @brief 싱글톤 패턴 객체 반환
+         *
+         * @return Arrow&
+         */
+        static Arrow &get_instance();
+
+        /**
+         * @brief 입력을 받는 함수
+         *
+         * @return void
+         */
+        inline void input();
+
+        /**
+         * @brief 화살이 맞히는 곳에 따른 점수를 계산해서 저장
+         *
+         * @return void
+         */
+        void set_arrow_scores();
+
+        /**
+         * @brief 목표 점수를 획득할 수 있는 가장 적은 화살의 필요 무게를 계산 및 출력
+         *
+         * @return void
+         */
+        void solve();
+    };
+
+    template <typename T>
+    Arrow<T> &Arrow<T>::get_instance()
+    {
+        static Arrow instance;
+        return instance;
+    }
+
+    template <typename T>
+    void Arrow<T>::input()
+    {
+        cin >> N >> M >> K;
+        board.resize(N, vector<T>(N));
+        weights.resize(M + 1);
+        for (auto &row : board)
+            for (auto &elem : row)
+                cin >> elem;
+        for (int i = 1; i <= M; i++) // 편의상 화살의 정보는 1부터 시작
+            cin >> weights[i];
+        return;
+    }
+
+    template <typename T>
+    void Arrow<T>::set_arrow_scores()
+    {
+        arrow_scores.resize(M + 1, vector<T>(N * N, 0));
+        for (int i = 1; i <= M; i++)                // 화살
+            for (int j = 0; j < N; j++)             // 화살이 맞히는 row
+                for (int k = 0; k < N; k++)         // 화살이 맞히는 column
+                    for (int l = 0; l < N; l++)     // 보드의 row
+                        for (int m = 0; m < N; m++) // 보드의 column
+                            if (abs(j - l) + abs(k - m) < i)
+                                arrow_scores[i][j * N + k] += board[l][m];
+        return;
+    }
+
+    template <typename T>
+    void Arrow<T>::solve()
+    {
+        input();
+        set_arrow_scores();
+        T INF = static_cast<T>(1e9);
+        dp.resize(M + 1, vector<T>(K + 1, INF));
+        dp[0][0] = 0;
+
+        for (int i = 1; i <= M; i++)
+        {
+            for (int j = 0; j <= K; j++)
+            {
+                if (j == 0)     // 목표 점수가 0일 때는 화살이 필요하지 않으며, 무게도 0이므로 0으로 초기화
+                    dp[i][j] = 0;
+                else
+                    for (int k = 0; k < N * N; k++)
+                        if (j - arrow_scores[i][k] >= 0)
+                            dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - arrow_scores[i][k]] + weights[i]); // INF, INF + weigths[i] 에는 INF가 저장되어 불가능을 뜻하게 됨
+            }
+        }
+
+        if(dp[M][K] == INF)
+            cout << -1 << '\n';
+        else
+            cout << dp[M][K] << '\n';
+
+        return;
+    }
+}
+
+using namespace AutoEver;
 
 int main()
 {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N, M, K; // 표적 크기, 화살 개수, 목표 점수
-    cin >> N >> M >> K;
+    auto &arrow = Arrow<int>::get_instance();
+    arrow.solve();
 
-    // 표적 점수 입력
-    vector<vector<int>> target(N, vector<int>(N));
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            cin >> target[i][j];
-
-    // 화살 무게 입력 화살은 1번 인덱스 부터 시작하도록
-    vector<int> weight(M + 1);
-    for (int i = 1; i <= M; i++)
-        cin >> weight[i];
-
-    // 각 화살로 얻을 수 있는 점수 계산
-    vector<vector<int>> arrow_scores(M + 1, vector<int>(N * N));
-    for (int i = 1; i <= M; i++)
-    {
-        int power = i;
-        int idx = 0;
-        for (int x = 0; x < N; x++) // 화살이 맞는 x좌표
-        {
-            for (int y = 0; y < N; y++) // 화살이 맞는 y좌표
-            {
-                // 거리 탐색해서 조건 맞으면 획득 가능 점수 계산해서 저장
-                int score = 0;
-                for (int nx = 0; nx < N; nx++)
-                    for (int ny = 0; ny < N; ny++)
-                        if (abs(nx - x) + abs(ny - y) < power)
-                            score += target[nx][ny];
-                arrow_scores[i][idx++] = score;
-            }
-        }
-    }
-
-    // 배낭 문제 기반 풀이
-    // 배낭 문제와의 차이점이라면 점수가 정확히 일치해야한다는 점임
-    vector<vector<int>> dp(M + 1, vector<int>(K + 1, INF));
-    for (int i = 1; i <= M; i++) // 화살 번호
-    {
-        for (int j = 0; j <= K; j++) // 점수
-        {
-            if (i == 1) // 첫 번째 화살은 그냥 넣어주기
-            {
-                for (int k = 0; k < N * N; k++)
-                    if (j == arrow_scores[i][k])    // 점수가 정확히 맞아야 함
-                        dp[i][j] = weight[i];
-            }
-            else
-            {
-                for (int k = 0; k < N * N; k++)
-                    if (j - arrow_scores[i][k] >= 0)
-                        dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - arrow_scores[i][k]] + weight[i]);
-            }
-        }
-    }
-
-    cout << (dp[M][K] == INF ? -1 : dp[M][K]) << "\n";
-
-    return 0;
+    return EXIT_SUCCESS;
 }
