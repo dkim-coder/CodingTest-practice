cb080b26371247c75a67fc48cf60fdbda7a81f47
dkim-coder <jeus5771@naver.com>
2025-03-31 17:21:56 +0900

    수정 필요

--- BaekJoon/pb_28215/main.cpp
+++ BaekJoon/pb_28215/main.cpp
@@ -1,54 +1,86 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int distReturn(pair<int, int> p1, pair<int, int> p2)
+int n, k;
+vector<bool> visited;
+int ans;
+
+inline int man_dist(const int &cr, const int &cc, const int &nr, const int &nc)
 {
-    return abs(p1.first - p2.first) + abs(p1.second - p2.second);
+    int ans = 0;
+
+    ans += (cr - nr) > 0 ? (cr - nr) : (nr - cr);
+    ans += (cc - nc) > 0 ? (cc - nc) : (nc - cc);
+
+    return ans;
+}
+
+void dfs(const int cnt, const int idx, const vector<vector<int>> &dist)
+{
+    if (cnt == k)
+    {
+        int max_dist = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i]) // 대피소 아닌곳
+            {
+                int tmp = INT32_MAX;
+                for (int j = 0; j < n; j++)
+                {
+                    if (visited[j])    // 대피소 인곳
+                    {
+                        tmp = min(tmp, dist[i][j]);
+                    }
+                }
+                max_dist = max(max_dist, tmp);
+            }
+        }
+        
+        ans = min(ans, max_dist);
+        
+        return;
+    }
+
+    for (int i = idx; i < n; i++)
+    {
+        visited[i] = true;
+        dfs(cnt + 1, i + 1, dist);
+        visited[i] = false;
+    }
+
+    return;
+}
+
+void solution()
+{
+    cin >> n >> k;
+
+    vector<pair<int, int>> houses(n);
+    vector<vector<int>> dist(n, vector<int>(n, 0));
+
+    for (int i = 0; i < n; i++)
+        cin >> houses[i].first >> houses[i].second;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            dist[i][j] = man_dist(houses[i].first, houses[i].second, houses[j].first, houses[j].second);
+
+    visited.resize(n, false);
+    ans = INT32_MAX;
+    dfs(0, 0, dist);
+    cout << ans << "\n";
+
+    return;
 }
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<pair<int, int>> houses;
-    vector<int> check(n, 1);
-    for (int i = 0; i < n; i++)
-    {
-        pair<int, int> tmp;
-        cin >> tmp.first >> tmp.second;
-        houses.push_back(tmp);
-    }
-    for (int i = 0; i < m; i++)
-        check[i] = 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int res = 999999;
-    do
-    {
-        int tmp = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (check[i])
-            { // 1 이면 대피소
-                for (int j = 0; j < n; j++)
-                {
-                    if (!check[j])
-                    {
-                        int dist = distReturn(houses[i], houses[j]);
-                        if (dist > tmp)
-                            tmp = dist;
-                    }
-                }
-            }
-        }
-        if (tmp < res)
-            res = tmp;
-    } while (next_permutation(check.begin(), check.end()));
+    solution();
 
-    cout << res << endl;
-
-    return EXIT_SUCCESS;
+    return 0;
 }
