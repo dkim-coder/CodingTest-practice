b28ce52356ae376383b77f54ee05af324bc25dc2
dkim-coder <jeus5771@naver.com>
2025-04-27 16:47:48 +0900

    복기 업데이트

--- RemindTest/DEEPX/draongs/main.cpp
+++ RemindTest/DEEPX/draongs/main.cpp
@@ -16,7 +16,6 @@ int solution(int n)
     {
         eggs[day] = eggs[day - 1]; // 이전 날 알수 로 현재 날의 알 수 업데이트
 
-        // 2일 지난 알 들은 부화
         if (day >= 2)
         {
             int hatched_eggs = 0;
@@ -26,29 +25,21 @@ int solution(int n)
             {
                 int breeding_dragons = 0; // 2일 전의 알 들의 부화
                 for (int age = 0; age < day - 2; age++)
-                    // 나이가 0~3일인 드래곤만 알을 낳을 수 있음
                     if (age < 4)
                         breeding_dragons += dragon_ages[day - 2][age];
                 hatched_eggs = breeding_dragons;
             }
 
-            // 알 감소, 나이 0인 드래곤 증가
-            eggs[day] -= hatched_eggs;
+            eggs[day] -= hatched_eggs;  // 부화한 알 수 만큼 알의 개수 감소 시켜야함
             dragon_ages[day][0] = hatched_eggs;
         }
 
-        // 3. 이전 날의 드래곤들 나이 증가
         for (int age = day - 1; age > 0; age--)
-        {
-            dragon_ages[day][age] = dragon_ages[day - 1][age - 1];
-        }
+            dragon_ages[day][age] = dragon_ages[day - 1][age - 1];  // 드래곤들의 나이 업데이트
 
-        // 4. 알을 낳을 수 있는 드래곤들이 알을 낳음 (나이 0~3인 드래곤)
         int new_eggs = 0;
-        for (int age = 0; age < 4 && age < day; age++)
-        {
+        for (int age = 0; age < 4 && age < day; age++)  // 드래곤 알 수 업데이트
             new_eggs += dragon_ages[day][age];
-        }
         eggs[day] += new_eggs;
     }
 
