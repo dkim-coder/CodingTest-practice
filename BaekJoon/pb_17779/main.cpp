be2ae2bed4d68d3b31154f1ad09e82b1148fd24f
dkim-coder <jeus5771@naver.com>
2025-09-14 19:46:16 +0900

    .

--- BaekJoon/pb_17779/main.cpp
+++ BaekJoon/pb_17779/main.cpp
@@ -1,93 +1,74 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
 using namespace std;
 
+const int INF = 987654321;
 int N;
-int A[21][21];
-int res;
-int TOTAL; // 전체 인구 합
+int map[21][21];
 
-bool isValid(int x, int y, int d1, int d2) {
-    return (x + d1 + d2 <= N && 1 <= y - d1 && y + d2 <= N);
+int diff(int x, int y, int d1, int d2) {
+	vector<int> population(5, 0);
+
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
+			//1번 선거구
+			if (r < x + d1 && c <= y && !(r >= x && c >= y - (r - x))) {
+				population[0] += map[r][c];
+			}
+			//2번 선거구
+			else if (r <= x + d2 && c > y && !(r >= x && c <= y + (r - x))) {
+				population[1] += map[r][c];
+			}
+			//3번 선거구
+			else if (r >= x + d1 && c < y - d1 + d2 && !(r <= x + d1 + d2 && c >= (y - d1 + d2 - (x + d1 + d2 - r)))) {
+				population[2] += map[r][c];
+			}
+			//4번 선거구
+			else if (r > x + d2 && c >= y - d1 + d2 && !(r <= x + d1 + d2 && c <= (y - d1 + d2 + (x + d1 + d2 - r)))) {
+				population[3] += map[r][c];
+			}
+			//5번 선거구
+			else {
+				population[4] += map[r][c];
+			}
+		}
+	}
+
+	sort(population.begin(), population.end());
+
+	return population[4] - population[0];
 }
 
-void update(int x, int y, int d1, int d2) {
-    static int area[21][21];
-    memset(area, 0, sizeof(area));
+int minDiff() {
+	int res = INF;
 
-    // 1) 경계선만 5로 표시
-    for (int i = 0; i <= d1; ++i) {
-        area[x + i][y - i] = 5;                 // 1번
-        area[x + d2 + i][y + d2 - i] = 5;       // 4번
-    }
-    for (int i = 0; i <= d2; ++i) {
-        area[x + i][y + i] = 5;                 // 2번
-        area[x + d1 + i][y - d1 + i] = 5;       // 3번
-    }
+	for (int x = 1; x <= N - 2; x++) {
+		for (int y = 2; y <= N - 1; y++) {
+			for (int d1 = 1; d1 <= y - 1 && d1 <= N - x - 1; d1++) {
+				for (int d2 = 1; d2 <= N - y && d2 <= N - x - 1; d2++) {
+					res = min(res, diff(x, y, d1, d2));
+				}
+			}
+		}
+	}
 
-    // 2) 각 구역 인구 (경계선 전까지만 누적)
-    int pop[6] = {0};
-
-    // 1번 구역: r = 1..x+d1-1, c = 1..y (왼쪽→오른쪽, 5를 만나면 그 행 종료)
-    for (int r = 1; r <= x + d1 - 1; ++r) {
-        for (int c = 1; c <= y; ++c) {
-            if (area[r][c] == 5) break;
-            pop[1] += A[r][c];
-        }
-    }
-
-    // 2번 구역: r = 1..x+d2, c = N..y+1 (오른쪽→왼쪽, 5를 만나면 그 행 종료)
-    for (int r = 1; r <= x + d2; ++r) {
-        for (int c = N; c >= y + 1; --c) {
-            if (area[r][c] == 5) break;
-            pop[2] += A[r][c];
-        }
-    }
-
-    // 3번 구역: r = x+d1..N, c = 1..(y-d1+d2-1) (왼쪽→오른쪽, 5를 만나면 종료)
-    for (int r = x + d1; r <= N; ++r) {
-        for (int c = 1; c <= y - d1 + d2 - 1; ++c) {
-            if (area[r][c] == 5) break;
-            pop[3] += A[r][c];
-        }
-    }
-
-    // 4번 구역: r = x+d2+1..N, c = N..(y-d1+d2) (오른쪽→왼쪽, 5를 만나면 종료)
-    for (int r = x + d2 + 1; r <= N; ++r) {
-        for (int c = N; c >= y - d1 + d2; --c) {
-            if (area[r][c] == 5) break;
-            pop[4] += A[r][c];
-        }
-    }
-
-    // 5번 구역 = 전체 - (1~4)
-    pop[5] = TOTAL - (pop[1] + pop[2] + pop[3] + pop[4]);
-
-    int mx = *max_element(pop + 1, pop + 6);
-    int mn = *min_element(pop + 1, pop + 6);
-    res = min(res, mx - mn);
+	return res;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    cin >> N;
-    TOTAL = 0;
-    for (int i = 1; i <= N; ++i)
-        for (int j = 1; j <= N; ++j) {
-            cin >> A[i][j];
-            TOTAL += A[i][j];
-        }
+	cin >> N;
+	for (int x = 1; x <= N; x++) {
+		for (int y = 1; y <= N; y++) {
+			cin >> map[x][y];
+		}
+	}
 
-    res = INT_MAX;
+	cout << minDiff() << endl;
 
-    for (int x = 1; x <= N; ++x)
-        for (int y = 1; y <= N; ++y)
-            for (int d1 = 1; d1 <= N; ++d1)
-                for (int d2 = 1; d2 <= N; ++d2)
-                    if (isValid(x, y, d1, d2))
-                        update(x, y, d1, d2);
-
-    cout << res << "\n";
-    return 0;
+	return 0;
 }
